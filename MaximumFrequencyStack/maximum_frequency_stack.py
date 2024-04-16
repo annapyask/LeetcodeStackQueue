968eb4eeb00c9b2ffbe16b5984f0b350c2433719
Anna Piaskovska <piaskovska.pn@ucu.edu.ua>
2024-04-16 17:51:55 +0300

    fixed Time Limit Exceeded

--- MaximumFrequencyStack/maximum_frequency_stack.py
+++ MaximumFrequencyStack/maximum_frequency_stack.py
@@ -1,26 +1,27 @@
-from collections import deque
+from collections import deque, defaultdict
+
 class FreqStack:
 
     def __init__(self):
         self.stack = deque()
+        self.dictio = defaultdict(int)
+        self.maximum = defaultdict(list)
+        self.count = 0
 
     def push(self, val: int) -> None:
         self.stack.appendleft(val)
+        self.dictio[val] += 1
+        num = self.dictio[val]
+        if num > self.count:
+            self.count = num
+        self.maximum[num].append(val)
 
     def pop(self) -> int:
-        counter = 0
-        indexx = 0
-        set_qu = set(self.stack)
-        if self.stack:
-            for i in set_qu:
-                if self.stack.count(i) == counter:
-                    if self.stack.index(i) < indexx:
-                        counter = self.stack.count(i)
-                        num = i
-                        indexx = self.stack.index(num)
-                if self.stack.count(i) > counter:
-                    counter = self.stack.count(i)
-                    num = i
-                    indexx = self.stack.index(num)
-            self.stack.remove(num)
-            return num
+        if not self.stack:
+            return None
+        val = self.maximum[self.count].pop()
+        if not self.maximum[self.count]:
+            self.count -= 1
+        self.dictio[val] -= 1
+        self.stack.remove(val)
+        return val
